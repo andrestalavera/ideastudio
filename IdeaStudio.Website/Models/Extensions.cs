5eccc88a243345e2907f72c46ff5f2e526c9189c
Andrés Talavera <andres.talavera@live.fr>
2025-08-30 22:08:39 +0200

    Before rework

--- IdeaStudio.Website/Models/Extensions.cs
+++ IdeaStudio.Website/Models/Extensions.cs
@@ -4,32 +4,37 @@ namespace IdeaStudio.Website.Models;
 
 public static class Extensions
 {
-	public static string ToSeoUrl(this string url)
-	=> url.ToLower()
-		.Replace("'", "-")
-		.Replace("é", "e")
-		.Replace("è", "e")
-		.Replace(" ", "-")
-		.Replace(".", "dot")
-		.Replace("#", "sharp")
-		.Replace("--", "-")
-		.Replace("--", "-")
-		.Replace("--", "-");
+	public static string ToSeoUrl(this string url) =>
+        url.ToLowerInvariant()
+           .Replace("'", "-")
+           .Replace("é", "e")
+           .Replace("è", "e")
+           .Replace(" ", "-")
+           .Replace(".", "dot")
+           .Replace("#", "sharp")
+           .Replace("--", "-");
 
-	public static Experiences? WithGeneratedIds(this Experiences experiences, ISeoService seoService)
-	{
-		if (experiences?.Items == null) return experiences;
+    public static Experiences? WithGeneratedIds(this Experiences? experiences, ISeoService seoService)
+    {
+        if (experiences?.Items == null) return experiences;
 
-		return experiences with
-		{
-			Items = [.. experiences.Items.Select((e, index) => e with
-			{
-				Id = seoService.GenerateId($"{e.Company} {e.Title}") ?? seoService.GenerateId($"experience-{index}")
-			})]
+        return experiences with
+        {
+            Items = [.. experiences.Items.Select((e, index) => e with
+            {
+                Id = seoService.GenerateId($"{e.Company} {e.Title}") ??
+                     seoService.GenerateId($"experience-{index}")
+            })]
 		};
+    }
+
+	public static Experience WithGeneratedIds(this Experience experience, ISeoService seoService)
+	{
+		var id = seoService.GenerateId(experience.CompanyAndTitle);
+		return experience with { Id = id };
 	}
 
-	public static TrainingCenters? WithGeneratedIds(this TrainingCenters trainingCenters, ISeoService seoService)
+    public static TrainingCenters? WithGeneratedIds(this TrainingCenters? trainingCenters, ISeoService seoService)
 	{
 		if (trainingCenters?.Items == null) return trainingCenters;
 
